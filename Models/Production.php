4bba83f159e992cd04da5bff793f4d028db65de5
MicheleIanni86 <micheleianni86@gmail.com>
2024-03-04 22:39:07 +0100

    feat: Added Function that Transforms the rating into Stars

--- Models/Production.php
+++ Models/Production.php
@@ -18,4 +18,15 @@ class Production {
             $this->voto = $voto;
             $this->genere = $genere;
     }
+
+    public function get_vote_star() {
+        $stars_html = '';
+        for ($i = 1; $i <= 5; $i++) {
+            $star_class = ($i <= $this->voto) ? 'fas' : 'far';
+            $stars_html .= '<i class="' . $star_class . ' fa-star"></i>';
+        }
+
+        return $stars_html;
+    }
 }
+    
