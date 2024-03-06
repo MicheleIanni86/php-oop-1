b26d3c23dfa70c4758882a24261fe26d56d75655
MicheleIanni86 <micheleianni86@gmail.com>
2024-03-06 15:12:44 +0100

    fix: Deleted Variables for Instances

--- db.php
+++ db.php
@@ -3,28 +3,24 @@ require_once __DIR__ . "./Models/Genre.php";
 require_once __DIR__ . "./Models/Movie.php";
 require_once __DIR__ . "./Models/TVSerie.php";
 
-$bud_terence = new Movie('lo chiamavano Trinità', 'it', 4, new Genre("Western", "Spaghetti Western fantastico"), "80k", 120);
 
-$di_caprio = new Movie('inception', 'us', 5, new Genre("Fantascienza", "Incredibilmente assurdo"), "120k", 160);
-
-$jack_nick = new Movie('shining', 'us', 3, new Genre("Horror", "Kubrick ci sapeva fare"), "60k", 115);
-
-$ant_hop = new Movie('il silenzio degli innocenti', 'en', 2, new Genre("Horror", "Il miglior Hopkins"), "110k", 145);
-
-$walt_white = new TVSerie('breaking bad', 'en', 5, new Genre("azione", "Storia davvero affascinante"), 5, 62 );
-
-$Jason_bat = new TVSerie('ozark', 'us', 3, new Genre("drammatico", "Vuoi fare soldi facili?"), 4, 44 );
 
 
 
 
 $videos = [
-    $bud_terence,
-    $di_caprio,
-    $jack_nick,
-    $ant_hop,
-    $walt_white,
-    $Jason_bat
+    
+    new Movie('lo chiamavano Trinità', 'it', 4, new Genre("Western", "Spaghetti Western fantastico"), "80k", 120),
+
+    new Movie('inception', 'us', 5, new Genre("Fantascienza", "Incredibilmente assurdo"), "120k", 160),
+    
+    new Movie('shining', 'us', 3, new Genre("Horror", "Kubrick ci sapeva fare"), "60k", 115),
+    
+    new Movie('il silenzio degli innocenti', 'en', 2, new Genre("Horror", "Il miglior Hopkins"), "110k", 145),
+    
+    new TVSerie('breaking bad', 'en', 5, new Genre("azione", "Storia davvero affascinante"), 5, 62 ),
+    
+    new TVSerie('ozark', 'us', 3, new Genre("drammatico", "Vuoi fare soldi facili?"), 4, 44 ),
 ];
 
 
